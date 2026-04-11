6f8b47c0900989f7b59d5c3028e52b0a310e5d77
Ashmit-Singh-Gogia <ashmit9955@gmail.com>
2026-04-11 19:34:59 +0000

    linux system memory parsing engine

--- internal/metrics/memory_models.go
+++ internal/metrics/memory_models.go
@@ -1,1 +1,27 @@
 package metrics
+
+type MemoryStats struct {
+	Total     uint64
+	Free      uint64
+	Used      uint64  // To be computed
+	UsedPer   float64 // To be computed
+	Available uint64
+	Buffers   uint64
+	Cached    uint64
+
+	SwapTotal uint64
+	SwapFree  uint64
+
+	Active   uint64
+	Inactive uint64
+	Dirty    uint64
+	Slab     uint64
+}
+
+type MemoryComputed struct {
+	Used             uint64
+	UsagePercent     float64
+	SwapUsed         uint64
+	SwapUsagePercent float64
+	Cache            uint64
+}
--- internal/metrics/memory_service.go
+++ internal/metrics/memory_service.go
@@ -1,1 +1,21 @@
 package metrics
+
+func Calculate_Mem_Usage(memStats MemoryStats) MemoryComputed {
+	// Calculate used and used per
+	var memoryComputed MemoryComputed
+	used := memStats.Total - memStats.Available
+	cache := memStats.Cached + memStats.Buffers
+	usedPer := float64(used) * 100 / float64(memStats.Total)
+	swapUsed := memStats.SwapTotal - memStats.SwapFree
+	if memStats.SwapTotal > 0 {
+		swapUsedPer := float64(swapUsed) * 100 / float64(memStats.SwapTotal)
+		memoryComputed.SwapUsagePercent = swapUsedPer
+	} else {
+		memoryComputed.SwapUsagePercent = 0
+	}
+	memoryComputed.Used = used
+	memoryComputed.UsagePercent = usedPer
+	memoryComputed.SwapUsed = swapUsed
+	memoryComputed.Cache = cache
+	return memoryComputed
+}
